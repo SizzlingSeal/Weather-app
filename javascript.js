3840e241cd02cbfb6ce56395e81bdae32818c05a
SizzlingSeal <77053239+SizzlingSeal@users.noreply.github.com>
2021-01-26 02:23:52 +0800

    Added code for the cities input

--- javascript.js
+++ javascript.js
@@ -10,4 +10,19 @@ fetch("./node_modules/all-countries-and-cities-json/countries.json")
         document.getElementById("countries").appendChild(node);
         
     }
+    document.getElementById("countries-input").addEventListener("focusout", () =>{
+        document.getElementById("cities").innerHTML = "";
+        var input = document.getElementById("countries-input").value;
+        console.log(json[input]);
+        var city = json[input];
+        for(var i = 0 ;i < city.length; i++){
+            var node = document.createElement("option");
+            node.setAttribute("value", `${city[i]}`);
+            document.getElementById("cities").appendChild(node);
+        }
+     });
+
 })
+
+
+
