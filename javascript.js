1e3b8543c22658fd89c5299dc495a63faca0ccb1
SizzlingSeal <77053239+SizzlingSeal@users.noreply.github.com>
2021-01-27 21:49:50 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,27 +1,9 @@
 var countriesList;
+//For getting the api later
 var cityName, countryCode;
 var apiKey ="dfdbfa904539422f381617199f857fac";
-
+//---------------------------------------------
 window.addEventListener("load", loadCountries());
-
-async function loadCountries() {
-    const resp = await fetch("./countries.json");
-    countriesList = await resp.json();
-    var countries = [];
-    for(var c in countriesList) countries.push(c);
-    for(var i =0; i < countries.length; i++){
-        var node = document.createElement("option");
-        node.setAttribute("value", `${countries[i]}`);
-        document.getElementById("countries").appendChild(node);
-    }
-}
-
-async function getIso(){
-    const resp =await fetch("./isoCountries.json");
-    var iso = await resp.json();
-    var input = document.getElementById("countries-input").value;
-    countryCode = iso[input];
-}
 
 document.getElementById("countries-input").addEventListener("change", () => {
     try{
@@ -41,7 +23,7 @@ document.getElementById("countries-input").addEventListener("change", () => {
 }
 });
 
-document.getElementById("countries-input").addEventListener("focusin", () =>{
+    document.getElementById("countries-input").addEventListener("focusin", () =>{
     document.getElementById("cities-input").disabled = true;
 });
 
@@ -50,13 +32,33 @@ async function getWeather(){
     cityName = document.getElementById("cities-input").value;
     const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=${apiKey}`);
     var weatherAPI = await resp.json();
-    console.log(weatherAPI);
     var cityID = weatherAPI.id;
 
     //openweathermap API widget
     if(weatherAPI.cod == "404"){
-        document.getElementById("openweathermap-widget-11").innerHTML ="<h2>Country not Found</h2>";
+        document.getElementById("openweathermap-widget-15").innerHTML ="<h2>Country not Found</h2>";
     }else{
+        var script = document.createElement('script');
+        var s = document.getElementsByTagName('script')[0];
+        script.async = true;
+        script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+    //two different weather widgets depending on the screen size
+    if(screen.width <= 700){ //widget id 15
+        document.getElementById("openweathermap-widget-15").innerHTML ="";
+    window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+    window.myWidgetParam.push({
+          id: 15,
+          cityid: cityID,
+          appid: 'dfdbfa904539422f381617199f857fac',
+          units: 'imperial',
+          containerid: 'openweathermap-widget-15',  
+        });
+    (function(){
+        s.parentNode.insertBefore(script, s);  
+    })();
+    }
+    else{//widget id 11
     document.getElementById("openweathermap-widget-11").innerHTML ="";
     window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
     window.myWidgetParam.push({
@@ -67,22 +69,31 @@ async function getWeather(){
           containerid: 'openweathermap-widget-11',  
         });
     (function(){
-    var script = document.createElement('script');
-    script.async = true;script.charset = "utf-8";
-    script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(script, s);  
+        s.parentNode.insertBefore(script, s);  
     })();
     }
-
+    }
     }catch (e){
     document.getElementById("openweathermap-widget-11").innerHTML ="";
-    console.log("Country not found");
+    document.getElementById("openweathermap-widget-15").innerHTML ="";
     }
-};
+}
 
+async function loadCountries() {
+    const resp = await fetch("./countries.json");
+    countriesList = await resp.json();
+    var countries = [];
+    for(var c in countriesList) countries.push(c);
+    for(var i =0; i < countries.length; i++){
+        var node = document.createElement("option");
+        node.setAttribute("value", `${countries[i]}`);
+        document.getElementById("countries").appendChild(node);
+    }
+}
 
-
-
-
-
+async function getIso(){
+    const resp =await fetch("./isoCountries.json");
+    var iso = await resp.json();
+    var input = document.getElementById("countries-input").value;
+    countryCode = iso[input];
+}
